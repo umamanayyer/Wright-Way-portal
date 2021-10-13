cd7931e67c567aa5fff2dcdc6941483ddf7548bf
umama <nayyerumama1@gmail.com>
2021-10-13 22:30:11 +0500

    added signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,25 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
 
   def show
     @user = User.find(params[:id])
   end
+
+  def create 
+    @user = User.new(user_params)
+    if user.save
+    flash[:notice] = "User Added !"
+    redirect_to users_path
+    else
+        render 'new'
+    end
+  end
+
+def user_params
+  params.require(:user).permit(:email, :password)
+end
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User <ApplicationRecord
+  before_save {self.email = email.downcase}
   VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, uniqueness: { case_sensitive: false }, presence: true, length: {maximum: 105}, format: {with: VALID_EMAIL_REGEX}
+  has_secure_password
 
 end
