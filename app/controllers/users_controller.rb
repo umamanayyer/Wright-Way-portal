de85d8388343aa6ad668cf2cae67d322178ab17a
umama <nayyerumama1@gmail.com>
2021-10-14 01:45:43 +0500

    completed functionality till listing users and adding users and editing users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,20 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
-
+  
   def show
     @user = User.find(params[:id])
   end
 
+  def index
+    @users = User.all
+  end
+  
+  def new
+    @user = User.new
+  end
+
   def create 
     @user = User.new(user_params)
-    if user.save
+    if @user.save
     flash[:notice] = "User Added !"
     redirect_to users_path
     else
@@ -17,8 +22,22 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update 
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+    flash[:notice] = "Your account information was successfully updated"
+    redirect_to users_path
+    else
+    render 'edit'
+    end
+end
+
 def user_params
-  params.require(:user).permit(:email, :password)
+  params.require(:user).permit(:email, :password_digest)
 end
 
 
