8577a4d6b23cce74b53c9c84dab0068f34f8e1dd
umama <nayyerumama1@gmail.com>
2021-10-14 12:57:46 +0500

    changed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
 end
 
 def user_params
-  params.require(:user).permit(:email, :password_digest)
+  params.require(:user).permit(:email, :password_digest, :first_name, :last_name, :prefered_name, :role, :status, :farm)
 end
 
 
